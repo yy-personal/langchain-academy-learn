8eff2fac28df682f493131d893250bf509d82335
Yin Yao Lim (NCS) <30777685+yy-personal@users.noreply.github.com>
2026-03-26 15:26:36 +0800

    completed module 1

--- module-1/studio/agent.py
+++ module-1/studio/agent.py
@@ -1,6 +1,6 @@
 from langchain_core.messages import SystemMessage
 from langchain_openai import AzureChatOpenAI
-
+import os
 from langgraph.graph import START, StateGraph, MessagesState
 from langgraph.prebuilt import tools_condition, ToolNode
 
@@ -34,7 +34,7 @@ def divide(a: int, b: int) -> float:
 tools = [add, multiply, divide]
 
 # Define LLM with bound tools
-llm = AzureChatOpenAI()
+llm = AzureChatOpenAI(azure_deployment=os.getenv("AZURE_OPENAI_CHAT_DEPLOYMENT_NAME"))
 llm_with_tools = llm.bind_tools(tools)
 
 # System message
